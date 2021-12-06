ece046a8d5436fb7249b8c67f9e6ab15cb97aa68
dk-sync <donghwa2428@gmail.com>
2021-12-06 13:22:34 +0900

    productview added

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls    import path, include
+
+from products.views import ProductView
 
 urlpatterns = [
-
+    path('/product', include([
+        path('', ProductView.as_view()),
+        path('/<int:product_id>', ProductView.as_view())
+    ])),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http        import JsonResponse
+from django.views       import View
+
+from products.models    import Product, Review
+
+class ProductView(View):
+      def get(self, request, **kwargs):
+
+          new_id = kwargs['product_id']
+
+          product_data = [{
+              'name' : product.name,
+              'ml_volume' : product.category.ml_volume,
+              'category_price' : product.category.price,
+              'thumb' : product.image_set.get(name='thumb').url,
+              'price' : [{
+                  'manufacturing_cost' : price.manufacturing_cost,
+                  'transportation_cost' : price.transportation_cost,
+                  'development_cost' : price.development_cost,
+                  'commision_cost' : price.commision_cost,
+                  'other_market_price' : price.other_market_price
+              } for price in product.price_set.all()],
+              'reviews' : [{
+                  'content' : review.content,
+                  'star' : review.star,
+                  'product_id' : review.product_id,
+                  'name' : review.user.name,
+                  'created_at' : review.created_at
+              } for review in product.review_set.all()],
+          } for product in Product.objects.all()]
+
+          return JsonResponse({'RESULT' : product_data[new_id], 'MESSAGE' : 'SUCCESS'}, status=200)
