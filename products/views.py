d017f6491ea7438653c6053bdc6f3f9efbc07bee
dk-sync <donghwa2428@gmail.com>
2021-12-07 15:52:40 +0900

    productview modified

--- products/views.py
+++ products/views.py
@@ -2,78 +2,52 @@ import json
 
 from django.http        import JsonResponse
 from django.views       import View
-from django.db.models   import Avg
+from django.db.models   import Avg, F
 
 from products.models    import Category, Product, Review, Price
 
 class ProductView(View):
       def get(self, request, **kwargs):
-#
-#        product_id = kwargs['product_id']
-#
-#        product_data = [{
-#            'name' : product.name,
-#            'ml_volume' : product.category.ml_volume,
-#            'category_price' : product.category.price,
-#            'thumb' : product.image_set.get(name='thumb').url,
-#            'tags' : [tag.name for tag in product.tags.all()],
-#            'reviews' : [{
-#                'content' : review.content,
-#                'star' : review.star,
-#                'product_id' : review.product_id,
-#                'name' : review.user.name,
-#                'created_at' : review.created_at,
-#
-#            } for review in product.review_set.all()],
-#            'price' : [{
-#                'manufacturing_cost' : price.manufacturing_cost,
-#                'transportation_cost' : price.transportation_cost,
-#                'development_cost' : price.development_cost,
-#                'commision_cost' : price.commision_cost,
-#                'other_market_price' : price.other_market_price
-#            } for price in product.price_set.all()]
-#        } for product in Product.objects.all()]
-#
-#        category_data = [{
-#            'average' : Review.objects.aggregate(Avg('star')),
-#            'products' :
-#                'tags' : [tag.name for tag in product.tags.all()],
-#                'reviews' : [{
-#                    'content' : review.content,
-#                    'star' : review.star,
-#                    'product_id' : review.product_id,
-#                    'name' : review.user.name,
-#                    'created_at' : review.created_at,
-#
-#                } for review in product.review_set.all()]
-#        } for product in Product.objects.all()]
-#        } for category in Category.objects.all()]
 
         product = Product.objects.get(id=kwargs['product_id'])
         category_id = product.category.id
-        price = Price.objects.get(id=kwargs['product_id'])
+        price = product.price_set.get()
+        review = [
+            *product.category.product_set.filter(review__isnull=False).annotate(
+                content=F('review__content'),
+                star=F('review__star'),
+                date=F('review__created_at'),
+                user_name=F('review__user__name')
+            ).values(
+                'id', 'content', 'star', 'date', 'user_name'
+            )
+        ]
 
         results = {
             'product_id': product.id,
             'ml_volume': product.category.ml_volume,
-            'thumb': product.image_set.filter(name='thumb')[0].url,
+            'images': [{
+                image.name: image.url
+            } for image in product.image_set.all()],
             'name': product.name,
             'category_price': product.category.price,
             'tags': [tag.name for tag in product.tags.all()],
-            'average_star': product.category.product_set.aggregate(star = Avg('review__star'))['star'],
-            'reviews' : [{
-                'content' : review.content,
-                'star' : review.star,
-                'name' : review.user.name,
-                'created_at' : review.created_at,
-             } for review in product.review_set.all()],
+            'average_star': float('%.1f' %(sum(map(lambda x: x['star'], review)) / len(review))) if len(review) != 0 else 0,
+            'review': review,
             'price': {
                 'manufacturing_cost': price.manufacturing_cost,
                 'transportation_cost': price.transportation_cost,
                 'development_cost': price.development_cost,
                 'commision_cost': price.commision_cost,
                 'other_market_price': price.other_market_price,
-            }
+            },
+            'product_set': [
+                {
+                    'id': product.id,
+                    'tags': [tag.name for tag in product.tags.all()]
+                }
+                for product in product.category.product_set.all()
+            ]
 
         }
 
